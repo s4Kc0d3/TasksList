438fe7fd508afae9b5477d5dab5d0f324bb5bd97
s4kc0d3 <iiborra@externos.itnow.es>
2022-06-27 16:04:29 +0200

    Updates

---
+++ precache-manifest.029409ebc4cef4f245f8e5586eda5406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb53a6afc4ca93c295a4",
+    "url": "/TasksList/static/css/main.e4e17e55.chunk.css"
+  },
+  {
+    "revision": "bb53a6afc4ca93c295a4",
+    "url": "/TasksList/static/js/main.bb53a6af.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/TasksList/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "aa09664b8ba24af008c2",
+    "url": "/TasksList/static/css/2.576e9be3.chunk.css"
+  },
+  {
+    "revision": "aa09664b8ba24af008c2",
+    "url": "/TasksList/static/js/2.aa09664b.chunk.js"
+  },
+  {
+    "revision": "f00f0953df98ebdbe137",
+    "url": "/TasksList/static/js/runtime~main.f00f0953.js"
+  },
+  {
+    "revision": "150fe899ca191e6d1281307ea6b96b2a",
+    "url": "/TasksList/index.html"
+  }
+];
